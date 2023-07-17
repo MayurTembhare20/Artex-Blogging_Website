fa262a1e23bbbea05f6a8ff009885d93f1245340
Mayur Tembhare <135987777+MayurTembhare20@users.noreply.github.com>
2023-07-17 20:45:28 +0530

    Final Commit
    This is the final commit

--- blog_post/settings.py
+++ blog_post/settings.py
@@ -78,18 +78,11 @@ WSGI_APPLICATION = 'blog_post.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-                'NAME': 'mayur',
-                'USER': 'root',
-                'HOST': 'localhost',
-                'PORT': '3306',
-                'PASSWORD': 'Maysql@202',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+       
 }
 
 
